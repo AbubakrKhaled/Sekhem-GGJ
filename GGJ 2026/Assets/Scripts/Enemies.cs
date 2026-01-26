0701f4016001c2e84cd804b7f4266854e8799fd0
shadysackla <shadysackla2006@gmail.com>
2026-01-26 20:36:46 +0200

    Enemies superclass and empty Mob subclass made

--- GGJ 2026/Assets/Scripts/Enemies.cs
+++ GGJ 2026/Assets/Scripts/Enemies.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
 
-public class Enemies : MonoBehaviour
+public abstract class Enemies : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // stats that show up in the inspector
+    public string enemyName;
+    public float health = 100f;
+    public float damage = 10f;
+    public float speed = 3f;
+    public float attackRange = 1.5f;
+    public float attackSpeed = 1f; // wait time between attacks
+
+    // hidden because we find it automatically in code
+    [HideInInspector]
+    public Transform player;
+
+    public virtual void Start()
     {
-        
+        // find the object named "Player" so we know who to chase
+        GameObject p = GameObject.Find("Player");
+        if (p != null)
+        {
+            player = p.transform;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(float amount)
     {
-        
+        // lower health
+        health -= amount;
+
+        // if health hits zero, destroy the object
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
+
+    // empty rule: every enemy must have an attack, but we don't know what it is yet
+    public abstract void Attack();
 }
